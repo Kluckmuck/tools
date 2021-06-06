2c45f6728c77ca1ce29672ed3971a22ca7be17ca
Kluckmuck <kluckmucki@gmail.com>
2021-06-06 14:06:38 +0200

    swapped frontend to angular. recreated react app in angular. cors setting changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,14 +43,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:4200'
 ]
--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import 'react-dates/initialize';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
+import momentPropTypes from 'react-moment-proptypes';
 
 function Details() {
     const [company, setCompany] = useState(Object);
@@ -11,42 +12,58 @@ function Details() {
     const [focused, setFocus] = useState();
 
     React.useEffect(() => {
-        // fetchData();
         const id = 1;
         axios
-            .get(`/api/businesses/${id}`)
-            // .then((res) => this.setCompany({ company: res.data }))
-            .then(data => {
-                setCompany(data.data);
-            })
-            .catch((err) => console.log(err));
+        .get(`/api/businesses/${id}`)
+        .then(data => {
+            setCompany(data.data);
+        })
+        .catch((err) => console.log(err));
     }, []);
-
+    
+    
     return (
-        <div>
+        <>
             <p>{company.name}</p>
             <p>{company.description}</p>
             <p>{company.location}</p>
+            {/* //<p>{date}</p> */}
             <Calendar date={date} focused={focused} setDate={setDate} setFocus={setFocus}/>
+            <Booking date={date}/>
             <Link to={`/`}>Back</Link>
-        </div>
+        </>
     )
 };
 
 function Calendar({ date, focused, setDate, setFocus}) {
+    console.log(Object.keys(date).length);
     return (
-        <div>
+        <>
             <SingleDatePicker
                 date={date}
-                onDateChange={(date) => {
-                    setDate(date)
-                    console.log(date)}
-                }
+                onDateChange={(date) => setDate(date)}
                 focused={focused}
                 onFocusChange={({ focused }) => setFocus(focused)}
                 id="date"
             />
-        </div>
+        </>
     );
 };
+
+function Booking(date) {
+    React.useEffect(() => {
+        console.log("date changed! " + date);
+    }, [date]);
+    if (date === null) {
+        return (
+            <div>
+                <button>Hej</button>
+            </div>
+        )
+    }
+    else return (
+        <p> NO BOOKING</p>
+    )
+}
+
 export default Details;
