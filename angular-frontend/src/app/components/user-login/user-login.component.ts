4faae562d373279e435405f30c86fdffe0c3c261
Kluckmuck <kluckmucki@gmail.com>
2021-10-24 15:13:05 +0200

    login page now uses formGroup and formBuilder. Should trigger chromes auto-saving of credentials

--- angular-frontend/src/app/components/user-login/user-login.component.ts
+++ angular-frontend/src/app/components/user-login/user-login.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-user-login',
-  templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.sass']
+  selector: "app-user-login",
+  templateUrl: "./user-login.component.html",
+  styleUrls: ["./user-login.component.sass"],
 })
 export class UserLoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   error: any;
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+    });
   }
 
-  login(username: string, password: string) {
-    this.authService.login(username, password).subscribe(
-      success => this.router.navigate(['start']),
-      error => this.error = error
-    );
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.authService
+      .login(this.f.username.value, this.f.password.value)
+      .subscribe(
+        (success) => this.router.navigate(["start"]),
+        (error) => (this.error = error)
+      );
   }
 }
